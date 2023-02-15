56c2debea8385426c851376ce83f3306b3eaeeb1
Julio Foulquie <jfoulquie@gmail.com>
2023-02-15 20:52:05 -0300

    Bind LocalActor models with 'user' parameter on routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\ActivityPub\LocalActor;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Http\Request;
@@ -32,6 +33,8 @@ class RouteServiceProvider extends ServiceProvider
             Route::middleware('federation')
                 ->group(base_path('routes/federation.php'));
         });
+
+        Route::model('user', LocalActor::class);
     }
 
     /**

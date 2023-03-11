6199b77a7a30234d5da7ef4be317fba8faae48e1
Julio Foulquie <jfoulquie@gmail.com>
2023-03-11 10:00:47 -0300

    improve docs

--- app/Http/Controllers/ActivityPub/Actors/NoteActivityController.php
+++ app/Http/Controllers/ActivityPub/Actors/NoteActivityController.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Controller;
 use App\Models\ActivityPub\LocalActor;
 use App\Models\ActivityPub\LocalNote;
 use App\Services\ActivityPub\Context;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class NoteActivityController extends Controller
@@ -16,11 +17,12 @@ class NoteActivityController extends Controller
      *
      * @param \Illuminate\Http\Request $request
      * @param \App\Models\ActivityPub\LocalActor $actor
-     * @param string $status
-     * @throws \Illuminate\Contracts\Container\BindingResolutionException
+     * @param \App\Models\ActivityPub\LocalNote $note
      * @return \Illuminate\Http\JsonResponse
+     * @throws \Exception
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
      */
-    public function __invoke(Request $request, LocalActor $actor, LocalNote $note)
+    public function __invoke(Request $request, LocalActor $actor, LocalNote $note) : JsonResponse
     {
         $context = ['@context' => [
             Context::ACTIVITY_STREAMS,

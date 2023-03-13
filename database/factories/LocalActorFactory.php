10df1b6b74151fe36a93443de05d91340351e372
Julio Foulquie <jfoulquie@gmail.com>
2023-03-13 00:32:23 -0300

    First db factories

---
+++ database/factories/LocalActorFactory.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\ActivityPub\LocalActor;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\ActivityPub\LocalActor>
+ */
+class LocalActorFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = LocalActor::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'created_at' => now(),
+            'updated_at' => now(),
+            'name' => fake()->name(),
+            'username' => fake()->userName(),
+            'avatar' => mb_substr(fake()->filePath() . '.jpg', 1),
+            'header' => mb_substr(fake()->filePath() . '.jpg', 1),
+            'bio' => fake()->text(),
+            'language' => fake()->languageCode(),
+            'activityId' => fake()->url(),
+            'url' => fake()->url(),
+            'type' => 'Service',
+            'actor_type' => 'local',
+        ];
+    }
+
+    /**
+     * Indicate that the model's email address should be unverified.
+     *
+     * @return static
+     */
+    public function unverified()
+    {
+        return $this->state(fn (array $attributes) => [
+            'email_verified_at' => null,
+        ]);
+    }
+}

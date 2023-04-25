7117b6125a8c8f011a54802bb172242c5105843d
Julio Foulquie <jfoulquie@gmail.com>
2023-04-24 22:11:13 -0300

    Fix note view

--- resources/views/bots/note.blade.php
+++ resources/views/bots/note.blade.php
@@ -4,7 +4,7 @@
 <div class="w-full flex flex-col justify-center m-4">
         @include('bots._note', ['avatar_size' => 'w-16'])
     <hr>
-@foreach ($note->replies->load('actor') as $reply)
+@foreach ($note->directReplies->load('actor') as $reply)
         @include('bots._note', [
             'name_class' => 'text-l truncate',
             'avatar_size' => 'w-10',

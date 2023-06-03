dd5eee51b2073e88a27dca087d5f02ab11f8656f
Julio Foulquie <jfoulquie@gmail.com>
2023-06-03 00:00:05 -0300

    Add response types

--- app/Http/Controllers/ActivityPub/Actors/TagController.php
+++ app/Http/Controllers/ActivityPub/Actors/TagController.php
@@ -4,11 +4,13 @@ namespace App\Http\Controllers\ActivityPub\Actors;
 
 use App\Http\Controllers\Controller;
 use App\Services\ActivityPub\Context;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class TagController extends Controller
 {
-    public function __invoke(Request $request, string $tag)
+    public function __invoke(Request $request, string $tag) : JsonResponse|View
     {
         if ($request->wantsJson()) {
             return response()->activityJson([

4427a0d55725f3beeba164bc194553d5433c424f
Julio Foulquie <jfoulquie@gmail.com>
2023-06-05 23:49:28 -0300

    type fixes

--- app/Processes/PublishPost.php
+++ app/Processes/PublishPost.php
@@ -4,12 +4,14 @@ declare(strict_types=1);
 
 namespace App\Processes;
 
+use App\Domain\Application\Note;
 use App\Jobs\Application\Posts\AddAttachments;
 use App\Jobs\Application\Posts\CreateNewPost;
 use App\Jobs\Application\Posts\ParseHashtags;
 use App\Jobs\Application\Posts\ParseLinks;
 use App\Jobs\Application\Posts\PublishPost as PostsPublishPost;
 use App\Jobs\Application\Posts\SchedulePost;
+use RuntimeException;
 
 final class PublishPost extends Process
 {
@@ -22,4 +24,18 @@ final class PublishPost extends Process
         SchedulePost::class,
         PostsPublishPost::class,
     ];
+
+    /**
+     *
+     * @param \App\Domain\Application\Note $note
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
+     * @return \App\Domain\Application\Note
+     */
+    public function run(object $note) : mixed
+    {
+        if (!$note instanceof Note) {
+            throw new RuntimeException('Invalid param for PublishPost process. An Application\\Note was expected');
+        }
+        return parent::run($note);
+    }
 }

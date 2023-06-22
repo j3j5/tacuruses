c37a3c189eabd1ba1816c92bd50eaf099455195f
Julio Foulquie <jfoulquie@gmail.com>
2023-06-22 18:54:20 -0300

    Add db factory for Follow model

---
+++ database/factories/ActivityPub/FollowFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Factories\ActivityPub;
+
+use App\Models\ActivityPub\Follow;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\ActivityPub\LocalActor>
+ */
+class FollowFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Follow::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'created_at' => now(),
+            'updated_at' => now(),
+            // 'actor_id' => '',
+            // 'target_id' => '',
+            'activityId' => fake()->url(),
+        ];
+    }
+}

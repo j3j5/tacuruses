d3641950e7288c77e9569e8247eb953a811f2791
Julio Foulquie <jfoulquie@gmail.com>
2024-04-15 20:07:30 -0300

    add tests for follow and unfollow

--- tests/Feature/Http/Controllers/ActivityPub/ActorInboxTest.php
+++ tests/Feature/Http/Controllers/ActivityPub/ActorInboxTest.php
@@ -4,9 +4,12 @@ namespace Tests\Feature\Http\Controllers\ActivityPub;
 
 use ActivityPhp\Type;
 use App\Enums\ActivityTypes;
+use App\Events\LocalActorFollowed;
+use App\Events\LocalActorUnfollowed;
 use App\Events\LocalNoteLiked;
 use App\Events\LocalNoteShared;
 use App\Models\ActivityPub\Activity;
+use App\Models\ActivityPub\Follow;
 use App\Models\ActivityPub\Like;
 use App\Models\ActivityPub\LocalActor;
 use App\Models\ActivityPub\LocalNote;
@@ -24,6 +27,137 @@ class ActorInboxTest extends TestCase
 {
 
     use LazilyRefreshDatabase, WithFaker;
+
+    public function test_follow()
+    {
+        /** @var \App\Models\ActivityPub\LocalActor $localActor */
+        $localActor = LocalActor::factory()->create();
+
+        $remoteActorKey = RSA::createKey()->withPadding(RSA::SIGNATURE_RELAXED_PKCS1);
+        /** @var \App\Models\ActivityPub\RemoteActor $remoteActor */
+        $remoteActor = RemoteActor::factory()
+            ->withPublicKey($remoteActorKey->getPublicKey()->toString('PKCS1'))
+            ->create();
+
+        $actorInfo = $this->actorResponse;
+        $actorInfo['id'] = $remoteActor->activityId;
+        $actorInfo['publicKey']['publicKeyPem'] = $remoteActor->publicKey;
+
+        Http::fake([
+            $actorInfo['id'] => Http::response($actorInfo, 200),
+            $actorInfo['publicKey']['id'] => Http::response($actorInfo, 200),
+            $actorInfo['inbox'] => Http::response('', 202),
+        ]);
+
+        $headers = [
+            'Accept' => 'application/activity+json',
+            'Content-Type' => 'application/activity+json',
+        ];
+
+        /** @var \ActivityPhp\Type\Extended\Activity\Like $dataLike */
+        $data = Type::create(ActivityTypes::FOLLOW->value, [
+            '@context' => Context::ACTIVITY_STREAMS,
+            'id' => $this->faker()->url,
+            'actor' => $actorInfo['id'],
+            'object' => $localActor->activityId,
+        ])->toArray();
+
+        Event::fake([
+            LocalActorFollowed::class,
+        ]);
+
+        $url = route('actor.inbox', [$localActor]);
+        $headers = $this->sign($remoteActorKey, $actorInfo['publicKey']['id'], $url, json_encode($data), $headers);
+        $response = $this->postJson($url, $data, $headers);
+
+        $response->assertAccepted();
+
+        $this->assertCount(1, $localActor->followers);
+        $this->assertTrue($localActor->followers->contains($remoteActor));
+        Event::assertDispatched(LocalActorFollowed::class);
+    }
+
+    public function test_undo_follow()
+    {
+        /** @var \App\Models\ActivityPub\LocalActor $localActor */
+        $localActor = LocalActor::factory()->create();
+
+        $remoteActorKey = RSA::createKey()->withPadding(RSA::SIGNATURE_RELAXED_PKCS1);
+        /** @var \App\Models\ActivityPub\RemoteActor $remoteActor */
+        $remoteActor = RemoteActor::factory()
+            ->withPublicKey($remoteActorKey->getPublicKey()->toString('PKCS1'))
+            ->create();
+
+        $actorInfo = $this->actorResponse;
+        $actorInfo['id'] = $remoteActor->activityId;
+        $actorInfo['publicKey']['publicKeyPem'] = $remoteActor->publicKey;
+
+        /** @var \ActivityPhp\Type\Extended\Activity\Follow $dataFollow */
+        $dataFollow = Type::create(ActivityTypes::FOLLOW->value, [
+            '@context' => Context::ACTIVITY_STREAMS,
+            'id' => $this->faker()->url,
+            'actor' => $actorInfo['id'],
+            'object' => $localActor->activityId,
+        ]);
+        /** @var \App\Models\ActivityPub\Activity $activity */
+        $activity = Activity::factory()
+            ->object($dataFollow)
+            ->accepted()
+            ->state([
+                'actor_id' => $remoteActor->id,
+                'target_id' => $localActor->id,
+            ])
+            ->create();
+
+        Follow::factory()
+            ->state(['activityId' => $activity->activityId])
+            ->accepted()
+            ->for($localActor, 'target')
+            ->for($remoteActor, 'actor')
+            ->create();
+
+        $object = $dataFollow->toArray();
+        unset($object['@context']);
+        $dataUndo = [
+            '@context' => Context::ACTIVITY_STREAMS,
+            'id' => $this->faker()->url,
+            'type' => ActivityTypes::UNDO->value,
+            'actor' => $actorInfo['id'],
+            'object' => $object,
+        ];
+
+        Http::fake([
+            $actorInfo['id'] => Http::response($actorInfo, 200),
+            $actorInfo['publicKey']['id'] => Http::response($actorInfo, 200),
+            $actorInfo['inbox'] => Http::response('', 202),
+        ]);
+
+        $headers = [
+            'Accept' => 'application/activity+json',
+            'Content-Type' => 'application/activity+json',
+        ];
+
+        Event::fake([
+            LocalActorUnfollowed::class,
+        ]);
+
+        $url = route('actor.inbox', [$localActor]);
+        $headers = $this->sign(
+            privateKey: $remoteActorKey,
+            keyId: $actorInfo['publicKey']['id'],
+            url: $url,
+            body: json_encode($dataUndo),
+            extraHeaders: $headers
+        );
+        $response = $this->postJson($url, $dataUndo, $headers);
+
+        $response->assertAccepted();
+        $localActor->refresh();
+
+        $this->assertCount(0, $localActor->followers);
+        $this->assertTrue($localActor->followers->doesntContain($remoteActor));
+        Event::assertDispatched(LocalActorUnfollowed::class);
+    }
 
     public function test_like_note()
     {

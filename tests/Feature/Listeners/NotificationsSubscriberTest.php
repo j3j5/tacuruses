f1efd9a817c340b007bf6f6d409fc9ce33e2c391
Julio Foulquie <jfoulquie@gmail.com>
2024-06-01 01:35:58 -0300

    Add reply test for notifications

--- tests/Feature/Listeners/NotificationsSubscriberTest.php
+++ tests/Feature/Listeners/NotificationsSubscriberTest.php
@@ -14,9 +14,11 @@ use App\Events\LocalNoteReplied;
 use App\Events\LocalNoteShared;
 use App\Listeners\NotificationsSubscriber;
 use App\Models\ActivityPub\LocalActor;
+use App\Models\ActivityPub\LocalNote;
 use App\Models\ActivityPub\RemoteActor;
 use App\Services\ActivityPub\Context;
 use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
+use Illuminate\Http\Response;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\Http;
@@ -59,9 +61,6 @@ class NotificationsSubscriberTest extends TestCase
         /** @var \App\Models\ActivityPub\LocalActor $localActor */
         $localActor = LocalActor::factory()->create();
         $remoteActor = RemoteActor::factory()->create();
-
-        /** @var \App\Models\ActivityPub\LocalActor $localActor */
-        $localActor = LocalActor::factory()->create();
 
         $remoteActorKey = RSA::createKey()->withPadding(RSA::SIGNATURE_RELAXED_PKCS1);
         /** @var \App\Models\ActivityPub\RemoteActor $remoteActor */
@@ -106,6 +105,63 @@ class NotificationsSubscriberTest extends TestCase
 
     public function test_reply_events_create_notification()
     {
-        $this->markTestIncomplete('todo');
+        /** @var \App\Models\ActivityPub\LocalActor $localActor */
+        $localActor = LocalActor::factory()->create();
+        $remoteActor = RemoteActor::factory()->create();
+
+        $remoteActorKey = RSA::createKey()->withPadding(RSA::SIGNATURE_RELAXED_PKCS1);
+        /** @var \App\Models\ActivityPub\RemoteActor $remoteActor */
+        $remoteActor = RemoteActor::factory()
+            ->withPublicKey($remoteActorKey->getPublicKey()->toString('PKCS1'))
+            ->create();
+
+        /** @var \App\Models\ActivityPub\LocalNote $note */
+        $note = LocalNote::factory()->public()->for($localActor, 'actor')->create();
+
+        $actorInfo = $remoteActor->getAPActor()->toArray();
+
+        Http::fake([
+            $actorInfo['id'] => Http::response($actorInfo),
+            $actorInfo['publicKey']['id'] => Http::response($actorInfo),
+            $actorInfo['inbox'] => Http::response('', 202),
+        ]);
+
+        $headers = [
+            'Accept' => 'application/activity+json',
+            'Content-Type' => 'application/activity+json',
+        ];
+
+        $activity = $this->generateCreateActivity($remoteActor);
+        $activity->cc = array_merge($activity->cc, [$localActor->activityId]);
+        $activity->object->inReplyTo = $note->activityId;
+        $activity->object->content = $localActor->canonical_username . ' ' . $activity->object->content;
+        $activity->object->tag = [[
+            'type' => 'Mention',
+            'href' => $localActor->activityId,
+            'name' => $localActor->canonical_username,
+        ]];
+
+        $headers = [
+            'Accept' => 'application/activity+json',
+            'Content-Type' => 'application/activity+json',
+        ];
+
+        $url = route('shared-inbox');
+        $data = $activity->toArray();
+
+        $headers = $this->sign($remoteActorKey, $remoteActor->publicKeyId, $url, json_encode($data), $headers);
+
+        Http::fake([
+            $remoteActor->inbox => Http::response('', Response::HTTP_ACCEPTED),
+        ]);
+        $response = $this->postJson($url, $data, $headers);
+        $response->assertAccepted();
+
+        $this->assertCount(1, $localActor->notifications);
+        $this->assertSame(NotificationTypes::REPLY, $localActor->notifications->first()->type);
+        $this->assertSame($remoteActor->name, Arr::get($localActor->notifications->first()->data, 'replace.user'));
+        $this->assertSame($remoteActor->canonical_username, Arr::get($localActor->notifications->first()->data, 'replace.username'));
+        $this->assertSame($remoteActor->domain, Arr::get($localActor->notifications->first()->data, 'replace.instance'));
+
     }
 }

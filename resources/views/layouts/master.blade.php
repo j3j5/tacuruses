7b4b02610a05688ee48e5ebd21f09ee35af0f38e
Julio Foulquie <jfoulquie@gmail.com>
2024-06-01 17:18:00 -0300

    All views from breeze (to be reviewed)

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,9 +1,10 @@
-<!DOCTYPE HTML>
-<html lang="en">
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta http-equiv="x-ua-compatible" content="ie=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
         @stack('head-meta')
 
@@ -14,7 +15,9 @@
         @stack('head-scripts')
     </head>
     <body class="font-sans text-base antialiased">
-        @yield('content')
+        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+            @yield('content')
+        </div>
 
         @stack('footer-scripts')
     </body>

dfb0a85440f50606a8ea77606fa08f671b3b8692
Julio Foulquie <jfoulquie@gmail.com>
2024-06-02 11:27:44 -0300

    Remove dark bg when device in dark mode

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -14,7 +14,7 @@
         <script src="{{ mix('js/app.js') }}" defer></script>
         @stack('head-scripts')
     </head>
-    <body class="font-sans text-base antialiased bg-gray-100 dark:bg-gray-900">
+    <body class="font-sans text-base antialiased bg-gray-100">
         <div class="min-h-screen">
             @yield('content')
         </div>

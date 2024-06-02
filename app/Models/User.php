d0449a3bcd4221a2cb14be6be307c25c6f6ec9a9
Julio Foulquie <jfoulquie@gmail.com>
2024-06-01 21:08:29 -0300

    Add admin support

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,6 +44,12 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'admin' => 'bool',
         ];
+    }
+
+    public function isAdmin() : bool
+    {
+        return $this->admin;
     }
 }

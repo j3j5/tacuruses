26269b1ae161237324839403181f97ccb55f357a
Julio Foulquie <jfoulquie@gmail.com>
2024-06-19 19:29:39 -0300

    rector & phpstan fixes

--- app/Livewire/Pulse/TopDeliveries.php
+++ app/Livewire/Pulse/TopDeliveries.php
@@ -1,17 +1,20 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Livewire\Pulse;
 
 use App\Enums\Pulse\RecordTypes;
 use App\Recorders\Deliveries;
 use Illuminate\Support\Facades\Config;
+use Illuminate\View\View;
 use Laravel\Pulse\Livewire\Card;
 use Livewire\Attributes\Lazy;
 
 #[Lazy]
 class TopDeliveries extends Card
 {
-    public function render()
+    public function render() : View
     {
         return view('livewire.pulse.top-deliveries', [
             'config' => Config::get('pulse.recorders.' . Deliveries::class),
--- app/Recorders/Deliveries.php
+++ app/Recorders/Deliveries.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Recorders;
 
 use App\Enums\Pulse\RecordTypes;

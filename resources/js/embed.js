808c4057251fed77f23be0064783c32a83ffaa7f
Julio Foulquie <jfoulquie@gmail.com>
2024-06-28 17:48:44 -0300

    Add embed.js and remove alpine from app.js

---
+++ resources/js/embed.js
@@ -0,0 +1,71 @@
+// Slightly modified Code from the Mastodon project
+// See https://github.com/mastodon/mastodon/blob/637de635054d42a0ae43035c2658c6941498f918/public/embed.js
+// License AGPLv3
+
+(function () {
+    'use strict';
+
+    /**
+     * @param {() => void} loaded
+     */
+    var ready = function (loaded) {
+      if (document.readyState === 'complete') {
+        loaded();
+      } else {
+        document.addEventListener('readystatechange', function () {
+          if (document.readyState === 'complete') {
+            loaded();
+          }
+        });
+      }
+    };
+
+    ready(function () {
+      /** @type {Map<number, HTMLIFrameElement>} */
+      var iframes = new Map();
+
+      window.addEventListener('message', function (e) {
+        var data = e.data || {};
+
+        if (typeof data !== 'object' || data.type !== 'setHeight' || !iframes.has(data.id)) {
+          return;
+        }
+
+        var iframe = iframes.get(data.id);
+
+        if ('source' in e && iframe.contentWindow !== e.source) {
+          return;
+        }
+
+        iframe.height = data.height;
+      });
+
+      [].forEach.call(document.querySelectorAll('iframe.tacuruses-embed'), function (iframe) {
+        // select unique id for each iframe
+        var id = 0, failCount = 0, idBuffer = new Uint32Array(1);
+        while (id === 0 || iframes.has(id)) {
+          id = crypto.getRandomValues(idBuffer)[0];
+          failCount++;
+          if (failCount > 100) {
+            // give up and assign (easily guessable) unique number if getRandomValues is broken or no luck
+            id = -(iframes.size + 1);
+            break;
+          }
+        }
+
+        iframes.set(id, iframe);
+
+        iframe.scrolling = 'no';
+        iframe.style.overflow = 'hidden';
+
+        iframe.onload = function () {
+          iframe.contentWindow.postMessage({
+            type: 'setHeight',
+            id: id,
+          }, '*');
+        };
+
+        iframe.onload();
+      });
+    });
+  })();
--- resources/views/actors/embed-note.blade.php
+++ resources/views/actors/embed-note.blade.php
@@ -11,55 +11,55 @@
     // See https://github.com/mastodon/mastodon/blob/4743657ba24e83c376e9f477fbf49114e6f09a57/app/javascript/mastodon/ready.js
     //     https://github.com/mastodon/mastodon/blob/4743657ba24e83c376e9f477fbf49114e6f09a57/app/javascript/entrypoints/public.tsx#L57
 
-/**
- * Ready function to make sure iframe is loaded to set the proper
- * height based on content.
- *
- * @param {(() => void) | (() => Promise<void>)} callback
- * @returns {Promise<void>}
- */
-function ready(callback) {
-    return new Promise((resolve, reject) => {
-        function loaded() {
-            let result;
-            try {
-                result = callback();
-            } catch (err) {
-                reject(err);
-                return;
+    /**
+    * Ready function to make sure iframe is loaded to set the proper
+    * height based on content.
+    *
+    * @param {(() => void) | (() => Promise<void>)} callback
+    * @returns {Promise<void>}
+    */
+    function ready(callback) {
+        return new Promise((resolve, reject) => {
+            function loaded() {
+                let result;
+                try {
+                    result = callback();
+                } catch (err) {
+                    reject(err);
+                    return;
+                }
+
+                if (typeof result?.then === 'function') {
+                    result.then(resolve).catch(reject);
+                } else {
+                    resolve();
+                }
             }
 
-            if (typeof result?.then === 'function') {
-                result.then(resolve).catch(reject);
+            if (['interactive', 'complete'].includes(document.readyState)) {
+                loaded();
             } else {
-                resolve();
+                document.addEventListener('DOMContentLoaded', loaded);
             }
-        }
-
-        if (['interactive', 'complete'].includes(document.readyState)) {
-            loaded();
-        } else {
-            document.addEventListener('DOMContentLoaded', loaded);
-        }
-    });
-}
-
-// Set the height when the message from the embedde page loads
-window.addEventListener('message', e => {
-    const data = e.data || {};
-
-    if (!window.parent || data.type !== 'setHeight') {
-        return;
+        });
     }
 
-    ready(() => {
-        window.parent.postMessage({
-        type: 'setHeight',
-        id: data.id,
-        height: document.getElementsByTagName('html')[0].scrollHeight,
-        }, '*');
+    // Set the height when the message from the embedde page loads
+    window.addEventListener('message', e => {
+        const data = e.data || {};
+
+        if (!window.parent || data.type !== 'setHeight') {
+            return;
+        }
+
+        ready(() => {
+            window.parent.postMessage({
+                type: 'setHeight',
+                id: data.id,
+                height: document.getElementsByTagName('html')[0].scrollHeight,
+            }, '*');
+        });
     });
-});
 </script>
 @endpush
 

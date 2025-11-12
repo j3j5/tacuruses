ece4f9dac83099227dced2f9f62b15c3db91b7f1
Julio Foulquie <jfoulquie@gmail.com>
2025-11-12 00:07:11 -0300

    Add ActivityUpdate child model

---
+++ app/Models/ActivityPub/ActivityUpdate.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Models\ActivityPub;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Parental\HasParent;
+
+/**
+ * App\Models\ActivityPub\ActivityUpdate
+ *
+ * @property int $id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string $activityId
+ * @property string $type
+ * @property int $actor_id
+ * @property int $target_id
+ * @property string|null $object_type
+ * @property array $object
+ * @property bool $accepted
+ * @property-read \App\Models\ActivityPub\Actor $actor
+ * @property-read string $slug
+ * @property-read \App\Models\ActivityPub\Note $target
+ * @method static \Illuminate\Database\Eloquent\Builder|ActivityCreate newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|ActivityCreate newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|ActivityCreate query()
+ * @method static \Illuminate\Database\Eloquent\Builder|ActivityCreate whereAccepted($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ActivityCreate whereActivityId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ActivityCreate whereActorId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ActivityCreate whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ActivityCreate whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ActivityCreate whereObject($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ActivityCreate whereObjectType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ActivityCreate whereTargetId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ActivityCreate whereType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|ActivityCreate whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
+class ActivityUpdate extends Activity
+{
+    use HasFactory;
+    use HasParent;
+
+    public function target() : BelongsTo
+    {
+        return $this->belongsTo(Note::class, 'target_id');
+    }
+}
